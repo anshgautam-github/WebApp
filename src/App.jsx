0422052f76b8f48e31b6b41f68c3dd0771b49491
Ansh Gautam <anshgautam1011@gmail.com>
2025-06-15 18:40:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
+import NavBar from './components/NavBar'
+import Feeatures from './components/Feeatures'
+import FloatingImage from './components/FloatingImage'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <NavBar/>
       <Hero />
-      <section className='z-0 min-h-screen bg-blue-500'/>
+      <About/>
+      <Feeatures/>
+      <FloatingImage/>
+      <Contact/>
+      <Footer/>
     </main>
   )
 }
