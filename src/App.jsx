9b34460bc0a9fec81d52065751ba7edfa22848fa
Ansh Gautam <anshgautam1011@gmail.com>
2025-06-15 14:12:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero'
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      <Hero/>
+      <Hero />
     </main>
   )
 }
